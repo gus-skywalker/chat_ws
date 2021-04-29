e8eedb72bbcc780dc5f2429438fd088509a9d234
gus-skywalker <gustavoxaviercontato@gmail.com>
2021-04-29 13:47:30 -0300

    hello world ;)

---
+++ src/test/java/com/tiia/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiia.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
